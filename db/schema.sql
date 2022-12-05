b24c8581f55d660cae20fc126fc61059fafe06ff
Calvin <84696281+caltran117@users.noreply.github.com>
2022-12-05 18:36:58 -0500

    sql updated

--- db/schema.sql
+++ db/schema.sql
@@ -6,20 +6,22 @@ USE employeetracker_db;
 CREATE TABLE department (
 	id INT auto_increment NOT NULL,
     name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
 	id INT auto_increment NOT NULL,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    airline_name VARCHAR(50) NOT NULL,
-    department_id (id)
+    department_id INT NOT NULL,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE employee (
-	id INT auto_increment NOT NULL,
+    id INT AUTO_INCREMENT NOT NULL,
     first_name VARCHAR(100) NOT NULL,
     last_name VARCHAR(100) NOT NULL,
     role_id INT NOT NULL,
-    manager_id INT
+    manager_id INT NULL,
+    PRIMARY KEY (id)
 );
--- db/seed.sql
+++ db/seed.sql
@@ -1,52 +1,25 @@
 USE employeetracker_db;
 
 INSERT INTO department
-(id, name) 
+(name) 
 VALUES 
-("Calvin", "Tran", "Spirit"),
-("Doe", "Boy", "United"),
-("trap", "dat", "Lufthansa Air");
+("HR"),
+("Technology");
 
 SELECT * FROM department;
 
-INSERT INTO role
-(id, title, salary, airline_name, department_id)
+INSERT INTO roledepartment
+(title, salary, department_id)
 VALUES
 
-('A100', 
-'2022-11-24 18:00',
-'2022-11-24 20:00',
-'EWR',
-'CIA',
-'ONTIME',
-1),
-('A200', 
-'2022-11-25 18:00',
-'2022-11-27 20:00',
-'EWR',
-'AAL',
-'ONTIME',
-3),
-('B2022', 
-'2022-11-25 18:00',
-'2022-11-27 20:00',
-'EWR',
-'HAV',
-'ONTIME',
-1),
-('C233', 
-'2022-11-20 18:00',
-'2022-11-21 20:00',
-'EWR',
-'NPE',
-'CANCELLED',
-2);
+("Manager", 80000.0, 1),
+("Manager", 90000.0, 2),
+("developer", 90000.0, 2);
 
 SELECT * FROM role;
 
 INSERT INTO employee
-(id, first_name, last_name, role_id, manager_id) 
+(first_name, last_name, role_id, manager_id) 
 VALUES 
-("Calvin", "Tran", "Spirit"),
-("Doe", "Boy", "United"),
-("trap", "dat", "Lufthansa Air");
+("Joe", "Smo", 2, null),
+("Calvin", "Tran", 3, 1);
