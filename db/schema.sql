d0460863206316ac516986f20dd2330e29e58f2f
Calvin <84696281+caltran117@users.noreply.github.com>
2022-12-05 17:29:59 -0500

    tables added in sql

--- db/schema.sql
+++ db/schema.sql
@@ -2,3 +2,24 @@ DROP DATABASE IF EXISTS employeetracker_db;
 CREATE DATABASE employeetracker_db;
 
 USE employeetracker_db;
+
+CREATE TABLE department (
+	id INT auto_increment NOT NULL,
+    name VARCHAR(30) NOT NULL,
+);
+
+CREATE TABLE role (
+	id INT auto_increment NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    airline_name VARCHAR(50) NOT NULL,
+    department_id (id)
+);
+
+CREATE TABLE employee (
+	id INT auto_increment NOT NULL,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT
+);
--- db/seed.sql
+++ db/seed.sql
@@ -1,1 +1,52 @@
 USE employeetracker_db;
+
+INSERT INTO department
+(id, name) 
+VALUES 
+("Calvin", "Tran", "Spirit"),
+("Doe", "Boy", "United"),
+("trap", "dat", "Lufthansa Air");
+
+SELECT * FROM department;
+
+INSERT INTO role
+(id, title, salary, airline_name, department_id)
+VALUES
+
+('A100', 
+'2022-11-24 18:00',
+'2022-11-24 20:00',
+'EWR',
+'CIA',
+'ONTIME',
+1),
+('A200', 
+'2022-11-25 18:00',
+'2022-11-27 20:00',
+'EWR',
+'AAL',
+'ONTIME',
+3),
+('B2022', 
+'2022-11-25 18:00',
+'2022-11-27 20:00',
+'EWR',
+'HAV',
+'ONTIME',
+1),
+('C233', 
+'2022-11-20 18:00',
+'2022-11-21 20:00',
+'EWR',
+'NPE',
+'CANCELLED',
+2);
+
+SELECT * FROM role;
+
+INSERT INTO employee
+(id, first_name, last_name, role_id, manager_id) 
+VALUES 
+("Calvin", "Tran", "Spirit"),
+("Doe", "Boy", "United"),
+("trap", "dat", "Lufthansa Air");
